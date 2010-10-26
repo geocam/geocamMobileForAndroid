8dc95939b39d43137595db4449f54d66b8a79592
trey.smith@nasa.gov <trey.smith@nasa.gov>
2010-10-26 09:32:23 -0700

    added copyright header

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
 
 APK=$1
 
